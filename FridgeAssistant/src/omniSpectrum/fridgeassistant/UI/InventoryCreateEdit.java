a866c05e9a9e96b9dc1529fb22b4ec261d40fe33
Ytseboy <danfreitaschn+programming@gmail.com>
2014-05-13 15:37:28 +0300

    Create Edit activity is now fully functional

--- FridgeAssistant/src/omniSpectrum/fridgeassistant/UI/InventoryCreateEdit.java
+++ FridgeAssistant/src/omniSpectrum/fridgeassistant/UI/InventoryCreateEdit.java
@@ -1,45 +1,36 @@
 package omniSpectrum.fridgeassistant.UI;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import omniSpectrum.fridgeassistant.Models.ItemDefinition;
 import omniSpectrum.fridgeassistant.R;
-import omniSpectrum.fridgeassistant.R.id;
-import omniSpectrum.fridgeassistant.R.layout;
-import omniSpectrum.fridgeassistant.R.menu;
 import omniSpectrum.fridgeassistant.Logic.DatabaseHelper;
 import omniSpectrum.fridgeassistant.Models.Category;
-import omniSpectrum.fridgeassistant.Models.ItemDefinition;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.os.Build;
 
 public class InventoryCreateEdit extends ActionBarActivity {
 	
 	DatabaseHelper db;
 	Spinner spinner;
+    EditText etName;
 	Button button;
-	EditText etName;
+    ArrayAdapter<Category> dataAdapter;
 	int id;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inventory_create_edit);
-		
 		//Retrieving Id from main activity
         Bundle extras = getIntent().getExtras();
 
@@ -53,21 +44,16 @@ public class InventoryCreateEdit extends ActionBarActivity {
         //Populate spinner with data from DB
         loadspinner();
 
+        //Checking if Create or Update
         if(extras != null){
             id = extras.getInt("itemId");
-            ItemDefinition myItem = db.getSingleItem(id);
-            Category myCategory = myItem.getItemCategory();
-            etName.setText(myItem.getName());
-            Toast.makeText(this, myItem.getItemCategory().toString(), Toast.LENGTH_SHORT).show();
-            spinner.setSelection(getIndex(spinner, myCategory));
-
+            if(id > 0){
+                ItemDefinition myItem = db.getSingleItem(id);
+                Category myCategory = myItem.getItemCategory();
+                etName.setText(myItem.getName(), TextView.BufferType.EDITABLE);
+                spinner.setSelection(getIndex(spinner, myCategory));
+            }
         }
-	
-//		
-//		if (savedInstanceState == null) {
-//			getSupportFragmentManager().beginTransaction()
-//					.add(R.id.container, new PlaceholderFragment()).commit();
-//		}
 	}
 
 	@Override
@@ -89,37 +75,19 @@ public class InventoryCreateEdit extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	//Method to populate Spinner
 	private void loadspinner() {
 		List<Category> categories = db.getAllCategories();
 		
-		ArrayAdapter<Category> dataAdapter = new ArrayAdapter<Category>(this, 
-				android.R.layout.simple_spinner_item, categories);
-		
-		spinner.setAdapter(dataAdapter);
+
+        dataAdapter = new ArrayAdapter<Category>(this,
+                android.R.layout.simple_spinner_item, categories);
+
+        spinner.setAdapter(dataAdapter);
 		
 	}
-	
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Toast.makeText(InventoryCreateEdit.this, "Hello", Toast.LENGTH_SHORT).show();
-        String name = etName.getText().toString();
-        Category category = (Category) spinner.getSelectedItem();
 
-        ItemDefinition newItem = new ItemDefinition(category,name);
-
-        if (newItem != null)
-        {
-            Toast.makeText(this, "New item is not null", Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, category.toString(), Toast.LENGTH_SHORT).show();
-            long dbItemDefinition = db.createItemDefinition(newItem);
-            finish();
-
-            //Toast.makeText(this, (int) dbItemDefinition, Toast.LENGTH_SHORT);
-        }
-	}
-	
     private int getIndex(Spinner spinner, Category myCategory){
 
         int index = 0;
@@ -132,21 +100,29 @@ public class InventoryCreateEdit extends ActionBarActivity {
         return index;
     }
 
-//	/**
-//	 * A placeholder fragment containing a simple view.
-//	 */
-//	public static class PlaceholderFragment extends Fragment {
-//
-//		public PlaceholderFragment() {
-//		}
-//
-//		@Override
-//		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//				Bundle savedInstanceState) {
-//			View rootView = inflater.inflate(
-//					R.layout.fragment_inventory_create_edit, container, false);
-//			return rootView;
-//		}
-//	}
+    //Handling event onClick
+	public void onClick(View v) {
+		String name = etName.getText().toString();
+        Category category = (Category) spinner.getSelectedItem();
 
+        ItemDefinition newItem;
+
+        //Check if create new or update
+        if (id > 0){
+            newItem = db.getSingleItem(id);
+            newItem.setItemCategory(category);
+            newItem.setName(name);
+            db.updateItemDefinition(newItem);
+
+        }
+        else {
+            newItem = new ItemDefinition(category, name);
+            db.createItemDefinition(newItem);
+        }
+
+        Toast.makeText(this, "Updated", Toast.LENGTH_SHORT).show();
+
+        finish();
+
+	}
 }
